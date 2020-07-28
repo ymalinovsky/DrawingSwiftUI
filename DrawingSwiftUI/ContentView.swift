06519977aace7e4e5a355b15782d6c9e3c7bcb54
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-07-28 07:35:14 +0300

    Initial Commit

---
+++ DrawingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawingSwiftUI
+//
+//  Created by Yan Malinovsky on 28.07.2020.
+//  Copyright © 2020 Yan Malinovsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
